41d52af89c0cc0498a2d6b119eb68f9f48184f9e
Jina Strickland <jina30@gmail.com>
2020-07-01 20:24:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def find_even_values(array_of_arrays)
     array_of_arrays[count]
     element_index = 0
     while element_index < array_of_arrays[count][element_index] do
-      if array_of_arrays[count][element_index] % 2 == 0
+      if array_of_arrays[count][element_index].even?
         p array_of_arrays[count][element_index]
       end
       element_index += 1
@@ -18,13 +18,23 @@ def find_even_values(array_of_arrays)
     count += 1
   end
 end
+
 #find_even_values(array_of_arrays)
 
-
-
-
-
-
+# def find_even_values(array_of_arrays)
+#   count = 0
+#   while count < array_of_arrays.length do
+#     array_of_arrays[count]
+#     element_index = 0
+#     while element_index < array_of_arrays[count][element_index] do
+#       if array_of_arrays[count][element_index] % 2 == 0
+#         p array_of_arrays[count][element_index]
+#       end
+#       element_index += 1
+#     end
+#     count += 1
+#   end
+# end
 
 
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
